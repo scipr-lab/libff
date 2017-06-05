94d83de3d941e53ecdd28cc417e72394fc30bec3
Aleksejs Popovs <aleksejs@popovs.lv>
2017-06-05 17:58:56 -0400

    Revert "Update FMT UNUSED case to resolve libsnark compilation issues". This change resulted in the type of FMT(...) being void when DEBUG is enabled, instead of char*. The libsnark compilation issue is actually unrelated to this, and should be resolved by removing the unnecessary  "libff::" from libsnark source code when calling FMT, as macros cannot be namespaced.
    This reverts commit 693fcc676d7ccb6d8bd98b05c1a8405ea910d1fd.

--- src/common/utils.hpp
+++ src/common/utils.hpp
@@ -45,7 +45,7 @@ void UNUSED(Types&&...) {}
 #ifdef DEBUG
 #define FMT FORMAT
 #else
-#define FMT UNUSED
+#define FMT(...) (UNUSED(__VA_ARGS__), "")
 #endif
 
 void serialize_bit_vector(std::ostream &out, const bit_vector &v);
