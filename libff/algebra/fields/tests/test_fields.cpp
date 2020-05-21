30f50b8cff3763f19e48b4f63e06c4bfed06b0f4
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-05-21 10:32:12 +0100

    Fix warnings in tests and support checks in release builds

--- libff/algebra/fields/tests/test_fields.cpp
+++ libff/algebra/fields/tests/test_fields.cpp
@@ -4,6 +4,13 @@
  *             and contributors (see AUTHORS).
  * @copyright  MIT license (see LICENSE file)
  *****************************************************************************/
+
+// If NDEBUG is defined, assert turns into nothing. No checks are made and a
+// lot of warnings are generated.
+#ifdef NDEBUG
+# undef NDEBUG
+#endif
+
 #include <libff/algebra/curves/edwards/edwards_pp.hpp>
 #include <libff/algebra/curves/mnt/mnt4/mnt4_pp.hpp>
 #include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
