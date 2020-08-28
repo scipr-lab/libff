e59cfc340b55c3d507999b0ea1317f3d587c91f1
Alexander Wu <alexanderwu68@yahoo.com>
2020-08-28 16:42:22 -0700

    Add files with the API for all fields, binary fields, and prime extension fields

---
+++ libff/algebra/fields/binary_field.hpp
@@ -0,0 +1,110 @@
+/** @file
+ *****************************************************************************
+ Declaration of common API for all finite fields in the binary/ directory.
+
+ Currently NOT used by the fields in this library. This class is not actually
+ the parent class of any field. All APIs are enforced through tests instead.
+
+ The reason for this is to ensure high performance of all fields. This class
+ exists as documentation for common API between fields.
+
+ Includes fields F2^n for specified n. All of the binary entension fields must
+ implement all functions declared in this class.
+ *****************************************************************************
+ * @author     This file is part of libff, developed by SCIPR Lab
+ *             and contributors (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+#include <libff/algebra/field_utils/bigint.hpp>
+#include <vector>
+
+namespace libff {
+
+template<typename T>
+class BinaryField;
+
+/* The type parameter T is intended to be set to the child class
+ * when this class is extended. For example,
+ * class gf32 : public BinaryField<gf32> ...
+ */
+template<typename T>
+class BinaryField {
+public:
+    /* Functions unique to binary fields */
+
+    static const constexpr uint64_t modulus_;
+    static const constexpr uint64_t num_bits;
+
+    /** generator of gf2^n */
+    static T multiplicative_generator;
+
+    /** If extension field, returns the base field's characteristic. */
+    template<mp_size_t n>
+    static constexpr bigint<n> field_char() { return bigint<n>(2); }
+
+    /* Functions common to all finite fields */
+
+#ifdef PROFILE_OP_COUNTS // NOTE: op counts are affected when you exponentiate with ^
+    static long long add_cnt;
+    static long long sub_cnt;
+    static long long mul_cnt;
+    static long long sqr_cnt;
+    static long long inv_cnt;
+#endif
+
+    virtual T& operator+=(const T& other) = 0;
+    virtual T& operator-=(const T& other) = 0;
+    virtual T& operator*=(const T& other) = 0;
+    virtual T& operator^=(const unsigned long pow) = 0;
+    template<mp_size_t m>
+    virtual T& operator^=(const bigint<m> &pow) = 0;
+
+    virtual T& square() = 0;
+    virtual T& invert() = 0;
+
+    virtual T operator+(const T& other) const;
+    virtual T operator-(const T& other) const;
+    virtual T operator*(const T& other) const;
+    virtual T operator^(const unsigned long pow) const;
+    template<mp_size_t m>
+    virtual T operator^(const bigint<m> &pow) const;
+    virtual T operator-() const = 0;
+
+    virtual T squared() const;
+    virtual T inverse() const;
+    /** HAS TO BE A SQUARE (else does not terminate). */
+    virtual T sqrt() const = 0;
+
+    bool operator==(const T& other) const = 0;
+    bool operator!=(const T& other) const = 0;
+    bool is_zero() const = 0;
+
+    void print() const = 0;
+    /**
+     * Returns the constituent bits in 64 bit words, in little-endian order.
+     * Only the right-most size_in_bits() bits are used; other bits are 0.
+     */
+    std::vector<uint64_t> as_words() const = 0;
+
+    void randomize() = 0;
+    void clear() = 0;
+
+    // the following should be defined in child classes, but are static so they can't be inherited
+    static T zero();
+    static T one();
+    static T random_element();
+    /**
+     * Creates a field element from the given bits in 64 bit words, in little-endian order.
+     * Only the right-most size_in_bits() bits are used; other bits are ignored.
+     */
+    static T from_words(std::vector<uint64_t> words);
+    static constexpr std::size_t extension_degree();
+    static std::size_t ceil_size_in_bits() { return num_bits; }
+    static std::size_t floor_size_in_bits();
+
+    // the following should be defined as well but can't be inherited
+    friend std::ostream& operator<<(std::ostream &out, const T &p);
+    friend std::istream& operator>>(std::istream &in, T &p);
+};
+
+} // libff
