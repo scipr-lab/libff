85577019a124e498db50ac97369acf169f50918d
Solomon Joseph <solomon.joseph@berkeley.edu>
2020-10-08 16:07:56 -0700

    Added clang-tidy functionality to CI and make target

---
+++ clang-tidy.cmake
@@ -0,0 +1,31 @@
+option(
+  USE_CLANG_TIDY
+  "Use clang-tidy if the program is found."
+  OFF
+)
+
+if(USE_CLANG_TIDY)
+  find_program(CLANG_TIDY clang-tidy)
+  if(CLANG_TIDY)
+    find_program(RUN_CLANG_TIDY run-clang-tidy.py)
+    if(RUN_CLANG_TIDY)
+      message("Using clang-tidy. Creating target... To run, use: make clang-tidy")
+      add_custom_target(
+        clang-tidy
+        COMMAND run-clang-tidy.py
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+        )
+      file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/api)
+      file(WRITE ${PROJECT_BINARY_DIR}/api/.clang-tidy
+        "Checks: '-*,misc-definitions-in-headers'
+CheckOptions:
+  - { key: HeaderFileExtensions,          value: \"x\" }")
+    else()
+      message(
+        FATAL_ERROR
+        "run-clang-tidy.py not found. (Download and place in PATH). Aborting...")
+    endif()
+  else()
+    message(FATAL_ERROR "clang-tidy not found. Aborting...")
+  endif()
+endif()
