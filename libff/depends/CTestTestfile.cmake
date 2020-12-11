5a779fb5b4cc50f7656e459f1491bd2c2b328675
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-12-11 09:29:12 +0000

    Removed local compilation files

--- libff/depends/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/valar/Code/git repos/libff/depends
-# Build directory: /Users/valar/Code/git repos/libff/libff/depends
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
