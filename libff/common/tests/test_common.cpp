f0c91100cbbb4ee85b0b8f7e84ca358f6662078e
Alexander Wu <alexanderwu68@yahoo.com>
2021-05-01 20:16:50 -0700

    Add more tests for common and field utils

--- libff/common/tests/test_common.cpp
+++ libff/common/tests/test_common.cpp
@@ -11,6 +11,9 @@
 #include <gtest/gtest.h>
 
 #include "libff/common/utils.hpp"
+#include "libff/algebra/fields/binary/gf32.hpp"
+#include "libff/algebra/curves/edwards/edwards_pp.hpp"
+#include "libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp"
 
 using namespace libff;
 
@@ -36,4 +39,49 @@ TEST(Log2Test, PowersOfTwo) {
         EXPECT_EQ(libff::log2(k), i);
         EXPECT_EQ(libff::log2(k+1), i+1);
     }
+}
+
+template<typename FieldT>
+void test_random_element()
+{
+    FieldT x = random_element_non_zero_one<FieldT>();
+    EXPECT_NE(x, FieldT::zero());
+    EXPECT_NE(x, FieldT::one());
+
+    x = random_element_non_zero<FieldT>();
+    EXPECT_NE(x, FieldT::zero());
+
+    FieldT y = random_element_exclude(x);
+    EXPECT_NE(x, y);
+}
+
+TEST(UtilsTest, RandomElementTest)
+{
+    init_edwards_fields();
+    test_random_element<edwards_Fq3>();
+    test_random_element<gf32>();
+}
+
+TEST(UtilsTest, CurveVectorSizeTest)
+{
+    init_edwards_params();
+    init_mnt6_params();
+
+    std::vector<edwards_G1> vec;
+
+    vec.push_back(edwards_G1::G1_one);
+    vec.push_back(edwards_G1::G1_zero);
+    vec.push_back(edwards_G1::G1_one);
+
+    EXPECT_EQ(curve_size_in_bits(vec), 552);
+
+    std::vector<mnt6_G2> vec2;
+
+    vec2.push_back(mnt6_G2::G2_zero);
+    vec2.push_back(mnt6_G2::G2_one);
+    vec2.push_back(mnt6_G2::G2_one);
+    vec2.push_back(mnt6_G2::G2_zero);
+    vec2.push_back(mnt6_G2::G2_zero);
+
+    EXPECT_EQ(curve_size_in_bits(vec2), 4475);
 }
